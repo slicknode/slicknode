d9dd8179f64b43b5f7d0375ea5ec1baa08d2b6ed
Ivo Meißner <info@overtronic.com>
2018-11-26 17:41:23 -0600

    Change default API endpoint to prod cluster

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
 /**
  * Created by Ivo Meißner on 11.01.18.
- *
- * @flow
  */
 
-export const DEFAULT_API_ENDPOINT = 'https://api.test.slicknode.com/';
+export const DEFAULT_API_ENDPOINT = 'https://api.slicknode.com/';
