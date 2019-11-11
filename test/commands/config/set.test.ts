b3a16954bb99f056053e476f5cfaf28817bf8d1b
Ivo Meißner <info@overtronic.com>
2019-11-11 14:40:34 -0600

    Increase test timeouts for config tests

--- test/commands/config/set.test.ts
+++ test/commands/config/set.test.ts
@@ -6,7 +6,7 @@ describe('config:set', () => {
   const TEST_URL = 'https://localhost';
   test
     .stdout()
-    .timeout(10000)
+    .timeout(20000)
     .stub(ConfigStorage.prototype, 'setValues', sinon.stub())
     .command(['config:set', 'endpoint', TEST_URL])
     .it('sets the endpoint', ctx => {
