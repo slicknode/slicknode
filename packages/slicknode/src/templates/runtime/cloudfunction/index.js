d98277152de1b96a874532c47cb77f4e91fe8369
Ivo Meißner <info@overtronic.com>
2021-11-05 13:18:34 -0500

    Add ESM module support fixes for cloudfunctions

--- packages/slicknode/src/templates/runtime/cloudfunction/index.js
+++ packages/slicknode/src/templates/runtime/cloudfunction/index.js
@@ -1,12 +1,12 @@
 /**
  * Slicknode Runtime Handler for Google Cloud Functions
  */
-const runtime = require('./runtime').default;
+import runtime from './runtime.js';
 
 /**
  * Process request
  */
-exports.function = async (req, res) => {
+export async function handler(req, res) {
   const result = await runtime.execute(JSON.stringify(req.body), req.headers);
   res.json(result);
-};
+}
