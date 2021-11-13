0626ef03990e8ee7cbcecd23877564b16d887db0
Ivo Meißner <info@overtronic.com>
2021-11-13 08:09:48 -0600

    Load test git repo via submodule (#57)
    * Allow project initialization from local filesystem git repo
    
    * Checkout submodules in github actions
    
    * Update submodules in CI
    
    * Prefer online dependencies in init from git
    
    * Remove submodule
    
    * Add project template submodule
    
    * Use CI project template in init command test
    
    * Remove obsolete submodule config

--- packages/slicknode/test/commands/init/init.test.ts
+++ packages/slicknode/test/commands/init/init.test.ts
@@ -3,9 +3,7 @@ import * as path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
 import { DefinitionNode, Kind, ObjectTypeDefinitionNode, parse } from 'graphql';
-import nock = require('nock');
 import { GET_REPOSITORY_URL_QUERY } from '../../../src/utils/pullDependencies';
-import os from 'os';
 
 const REGISTRY_URL = 'http://localhost/repository/';
 const registryUrlResult = {
@@ -14,6 +12,7 @@ const registryUrlResult = {
 
 describe('init', () => {
   const EMPTY_DIR = path.join(__dirname, 'testprojects', 'empty');
+  const REPO_PATH = path.join(__dirname, 'fixtures', 'project-repo');
 
   test
     .stdout()
@@ -208,211 +207,199 @@ describe('init', () => {
     .catch(/Error checking out git reference "invalidreference"/)
     .it('fails for invalid git reference', (ctx) => {});
 
-  // @TODO: Figure out why this fails on windows and make it pass
-  if (process.platform !== 'win32') {
-    mockRegistry(test)
-      .stdout()
-      .stderr()
-      .cliActions([
-        'Loading project template "https://github.com/slicknode/starter-nextjs-blog.git"',
-        'Installing node dependencies',
-        'Updating dependencies',
-      ])
-      .login()
-      .api(GET_REPOSITORY_URL_QUERY, registryUrlResult)
-      .timeout(180000)
-      .workspaceCommand(EMPTY_DIR, [
-        'init',
-        'test-dir',
-        'https://github.com/slicknode/starter-nextjs-blog.git',
-      ])
-      .it('initializes project successfully from template URL', (ctx) => {
-        // Check slicknode.yml content
-        const slicknodeYml = yaml.safeLoad(
-          fs
-            .readFileSync(
-              path.join(ctx.workspace!, 'test-dir', 'slicknode.yml')
-            )
-            .toString()
-        );
-        expect(slicknodeYml).to.deep.equal({
-          dependencies: {
-            '@private/blog': './modules/blog',
-            auth: 'latest',
-            content: 'latest',
-            core: 'latest',
-            image: 'latest',
-            relay: 'latest',
-          },
-        });
-
-        // Check if core modules were added to cache
-        const coreModuleYml = yaml.safeLoad(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                '.slicknode',
-                'cache',
-                'modules',
-                'core',
-                'slicknode.yml'
-              )
-            )
-            .toString()
-        );
-        expect(coreModuleYml).to.deep.equal({
-          module: { id: 'core', label: 'Core' },
-        });
-
-        // Check schema.graphql of core module
-        const coreSchema = parse(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                '.slicknode',
-                'cache',
-                'modules',
-                'core',
-                'schema.graphql'
-              )
-            )
-            .toString()
-        );
-        expect(coreSchema.kind).to.equal(Kind.DOCUMENT);
-        expect(coreSchema.definitions.length).to.be.above(5);
-
-        expect(ctx.stdout).to.contain('Project initialized');
-
-        // Check if blog module was downloaded from git
-        const blogSchema = parse(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                'modules',
-                'blog',
-                'schema.graphql'
-              )
-            )
-            .toString()
-        );
-        expect(blogSchema.kind).to.equal(Kind.DOCUMENT);
-        expect(blogSchema.definitions.length).to.be.above(1);
-
-        const postType: DefinitionNode = blogSchema
-          .definitions[0] as ObjectTypeDefinitionNode;
-        expect(postType.kind).to.equal(Kind.OBJECT_TYPE_DEFINITION);
-        expect(postType.name.value).to.equal('Blog_Post');
-
-        // Check if node_modules was installed
-        const nextPackageJson = JSON.parse(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                'node_modules',
-                'next',
-                'package.json'
-              )
-            )
-            .toString()
-        );
-        expect(nextPackageJson.name).to.equal('next');
+  mockRegistry(test)
+    .stdout()
+    .stderr()
+    .cliActions([
+      `Loading project template "${REPO_PATH}"`,
+      'Installing node dependencies',
+      'Updating dependencies',
+    ])
+    .login()
+    .api(GET_REPOSITORY_URL_QUERY, registryUrlResult)
+    .timeout(180000)
+    .workspaceCommand(EMPTY_DIR, ['init', 'test-dir', REPO_PATH])
+    .it('initializes project successfully from template URL', (ctx) => {
+      // Check slicknode.yml content
+      const slicknodeYml = yaml.safeLoad(
+        fs
+          .readFileSync(path.join(ctx.workspace!, 'test-dir', 'slicknode.yml'))
+          .toString()
+      );
+      expect(slicknodeYml).to.deep.equal({
+        dependencies: {
+          '@private/blog': './modules/blog',
+          auth: 'latest',
+          content: 'latest',
+          core: 'latest',
+          image: 'latest',
+          relay: 'latest',
+        },
       });
 
-    mockRegistry(test)
-      .stdout()
-      .stderr()
-      .cliActions([
-        'Loading project template',
-        'Installing node dependencies',
-        'Updating dependencies',
-      ])
-      .login()
-      .api(GET_REPOSITORY_URL_QUERY, registryUrlResult)
-      .timeout(180000)
-      .workspaceCommand(EMPTY_DIR, [
-        'init',
-        'test-dir',
-        'https://github.com/slicknode/starter-nextjs-blog.git#b3262fc1ac2793e7199ba8b2b546d767c45ad4e2',
-      ])
-      .it('initializes from commit hash successfully', (ctx) => {
-        // Check slicknode.yml content
-        const slicknodeYml = yaml.safeLoad(
-          fs
-            .readFileSync(
-              path.join(ctx.workspace!, 'test-dir', 'slicknode.yml')
+      // Check if core modules were added to cache
+      const coreModuleYml = yaml.safeLoad(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              '.slicknode',
+              'cache',
+              'modules',
+              'core',
+              'slicknode.yml'
             )
-            .toString()
-        );
-        expect(slicknodeYml).to.deep.equal({
-          dependencies: {
-            auth: 'latest',
-            core: 'latest',
-            relay: 'latest',
-          },
-        });
-
-        // Check if core modules were added to cache
-        const coreModuleYml = yaml.safeLoad(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                '.slicknode',
-                'cache',
-                'modules',
-                'core',
-                'slicknode.yml'
-              )
-            )
-            .toString()
-        );
-        expect(coreModuleYml).to.deep.equal({
-          module: { id: 'core', label: 'Core' },
-        });
-
-        // Check schema.graphql of core module
-        const coreSchema = parse(
-          fs
-            .readFileSync(
-              path.join(
-                ctx.workspace!,
-                'test-dir',
-                '.slicknode',
-                'cache',
-                'modules',
-                'core',
-                'schema.graphql'
-              )
-            )
-            .toString()
-        );
-        expect(coreSchema.kind).to.equal(Kind.DOCUMENT);
-        expect(coreSchema.definitions.length).to.be.above(5);
-
-        expect(ctx.stdout).to.contain('Project initialized');
-
-        // Check if node_modules was installed
-        const packageJson = JSON.parse(
-          fs
-            .readFileSync(path.join(ctx.workspace!, 'test-dir', 'package.json'))
-            .toString()
-        );
-        expect(packageJson.dependencies).to.deep.equal({
-          next: '^9.5.3',
-          react: '^16.13.1',
-          'react-dom': '^16.13.1',
-        });
+          )
+          .toString()
+      );
+      expect(coreModuleYml).to.deep.equal({
+        module: { id: 'core', label: 'Core' },
       });
-  }
+
+      // Check schema.graphql of core module
+      const coreSchema = parse(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              '.slicknode',
+              'cache',
+              'modules',
+              'core',
+              'schema.graphql'
+            )
+          )
+          .toString()
+      );
+      expect(coreSchema.kind).to.equal(Kind.DOCUMENT);
+      expect(coreSchema.definitions.length).to.be.above(5);
+
+      expect(ctx.stdout).to.contain('Project initialized');
+
+      // Check if blog module was downloaded from git
+      const blogSchema = parse(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              'modules',
+              'blog',
+              'schema.graphql'
+            )
+          )
+          .toString()
+      );
+      expect(blogSchema.kind).to.equal(Kind.DOCUMENT);
+      expect(blogSchema.definitions.length).to.equal(1);
+
+      const postType: DefinitionNode = blogSchema
+        .definitions[0] as ObjectTypeDefinitionNode;
+      expect(postType.kind).to.equal(Kind.OBJECT_TYPE_DEFINITION);
+      expect(postType.name.value).to.equal('Blog_Page');
+
+      // Check if node_modules was installed
+      const nextPackageJson = JSON.parse(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              'node_modules',
+              'graphql',
+              'package.json'
+            )
+          )
+          .toString()
+      );
+      expect(nextPackageJson.name).to.equal('graphql');
+    });
+
+  mockRegistry(test)
+    .stdout()
+    .stderr()
+    .cliActions([
+      'Loading project template',
+      'Installing node dependencies',
+      'Updating dependencies',
+    ])
+    .login()
+    .api(GET_REPOSITORY_URL_QUERY, registryUrlResult)
+    .timeout(180000)
+    .workspaceCommand(EMPTY_DIR, [
+      'init',
+      'test-dir',
+      `${REPO_PATH}#94ded0d013ba309a90023ad48503c038f68918dd`,
+    ])
+    .it('initializes from commit hash successfully', (ctx) => {
+      // Check slicknode.yml content
+      const slicknodeYml = yaml.safeLoad(
+        fs
+          .readFileSync(path.join(ctx.workspace!, 'test-dir', 'slicknode.yml'))
+          .toString()
+      );
+      expect(slicknodeYml).to.deep.equal({
+        dependencies: {
+          '@private/blog': './modules/blog',
+          auth: 'latest',
+          core: 'latest',
+          relay: 'latest',
+        },
+      });
+
+      // Check if core modules were added to cache
+      const coreModuleYml = yaml.safeLoad(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              '.slicknode',
+              'cache',
+              'modules',
+              'core',
+              'slicknode.yml'
+            )
+          )
+          .toString()
+      );
+      expect(coreModuleYml).to.deep.equal({
+        module: { id: 'core', label: 'Core' },
+      });
+
+      // Check schema.graphql of core module
+      const coreSchema = parse(
+        fs
+          .readFileSync(
+            path.join(
+              ctx.workspace!,
+              'test-dir',
+              '.slicknode',
+              'cache',
+              'modules',
+              'core',
+              'schema.graphql'
+            )
+          )
+          .toString()
+      );
+      expect(coreSchema.kind).to.equal(Kind.DOCUMENT);
+      expect(coreSchema.definitions.length).to.be.above(5);
+
+      expect(ctx.stdout).to.contain('Project initialized');
+
+      // Check if node_modules was installed
+      const packageJson = JSON.parse(
+        fs
+          .readFileSync(path.join(ctx.workspace!, 'test-dir', 'package.json'))
+          .toString()
+      );
+      expect(packageJson.dependencies).to.deep.equal({
+        graphql: '^16.0.1',
+      });
+    });
 });
 
 function mockRegistry(t: typeof test) {
